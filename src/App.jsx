8fc76e587808ac5ee7396d78f63cd865a8218424
Badri vishal singh <badri.singh8090@gmail.com>
2023-12-09 16:52:28 +0530

    deployed now

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import Contact from './components/contact/Contact';
-import Experience from './components/experience/Experience';
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
-import Intro from './components/intro/Intro';
-import Portfolio from './components/portfolio/Portfolio';
-// import Testimonials from './components/testimonials/Testimonials';
-import Topbar from './components/topbar/Topbar';
-
+import React from "react";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Topbar from "./components/topbar/Topbar";
 
 const App = () => {
   return (
@@ -17,11 +15,10 @@ const App = () => {
       <Intro />
       <Experience />
       <Portfolio />
-      {/* <Testimonials /> */}
       <Contact />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
