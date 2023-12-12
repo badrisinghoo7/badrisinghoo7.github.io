c28cc5bfb3c35526f1355f498f11da0be6d929f4
Badri vishal singh <badri.singh8090@gmail.com>
2023-12-13 00:47:02 +0530

    Finishing this

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# badrisinghoo7.github.io
+# Portfolio 
+
+This repository contains the source code and assets for my personal portfolio website. The website showcases my skills, projects, and contact information.
+
+## Live Demo
+
+Check out the live version of my portfolio website: [AK](https://ak016.github.io/)
+![Demo](https://github.com/AK016/AK016.github.io/assets/123861375/e0f6d4fd-cfa6-4e8b-9d1d-1a38424118cd)
+
+
+
+## Features
+
+- Responsive design for optimal viewing on various devices
+- Smooth scrolling navigation with active section highlighting
+- Dynamic job title animation
+- Skills and projects displayed in an appealing grid layout
+- Contact information provided for easy communication
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
+- jQuery
+
+## Usage
+
+1. Clone the repository: `git clone https://github.com/AK016/AK016.github.io.git
+2. Open the `index.html` file in your web browser.
+3. Explore the different sections of the portfolio website.
+4. Click on the navigation links to scroll to the corresponding sections.
+5. Contact me using the provided contact information.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Contact
+
+If you have any questions or feedback, feel free to reach out to me:
+
+- Email: akshaykadam9010@gmail.com
+- LinkedIn: https://www.linkedin.com/in/akshay-kadam-8444b81ab/
+- Phone: +91 9686315358
--- index.js
+++ index.js
@@ -1,56 +1,324 @@
 // Smooth scroll to section
-function smoothScroll(target, duration) {
-  const targetSection = document.querySelector(target);
-  if (targetSection) {
-    const targetPosition = targetSection.getBoundingClientRect().top;
-    const startPosition = window.pageYOffset;
-    const distance = targetPosition - startPosition;
-    let startTime = null;
+const navLinks = document.querySelectorAll("#nav-menu a:not(.resume-link)");
 
-    function scrollAnimation(currentTime) {
-      if (startTime === null) startTime = currentTime;
-      const timeElapsed = currentTime - startTime;
-      const scrollY = ease(timeElapsed, startPosition, distance, duration);
-      window.scrollTo(0, scrollY);
-      if (timeElapsed < duration) requestAnimationFrame(scrollAnimation);
+// Function to add the "active" class to the navigation link of the currently active section
+function highlightActiveSection() {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav-link:not(.resume)");
+
+  let currentActiveIndex = -1;
+
+  sections.forEach((section, index) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top <= 100 && rect.bottom >= 100) {
+      currentActiveIndex = index;
     }
+  });
 
-    function ease(t, b, c, d) {
-      t /= d / 2;
-      if (t < 1) return c / 2 * t * t + b;
-      t--;
-      return -c / 2 * (t * (t - 2) - 1) + b;
+  navLinks.forEach((link, index) => {
+    if (index === currentActiveIndex) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
     }
-
-    requestAnimationFrame(scrollAnimation);
-  }
+  });
 }
 
+// Call the main event handler when the page loads and when the user scrolls
+window.addEventListener("load", () => {
+  highlightActiveSection();
+});
 
+window.addEventListener("scroll", () => {
+  highlightActiveSection();
+});
 
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
-const projectContainer = document.querySelector('.project-card-container');
+// Call the functions again after a delay in case the page content changes dynamically
+setInterval(() => {
+  highlightActiveSection();
+}, 1000); // Adjust the delay time (1000 milliseconds = 1 second) as needed
 
-let scrollPosition = 0;
-const projectWidth = 320; // Adjust this value to match the width of each project card
-const projectsToShow = 3; // Adjust this value to change the number of projects displayed at a time
+const toggleButton = document.getElementById("nav-toggle-button");
+const navList = document.getElementById("nav-links");
 
-prevBtn.addEventListener('click', scrollProjects.bind(null, 'prev'));
-nextBtn.addEventListener('click', scrollProjects.bind(null, 'next'));
+// Function to toggle the 'active' class for the nav-list
+function toggleNavMenu() {
+  navList.classList.toggle("active");
+}
 
-function scrollProjects(direction) {
-  const containerWidth = projectContainer.offsetWidth;
-  const scrollAmount = projectWidth * projectsToShow;
-  const maxScroll = projectContainer.scrollWidth - containerWidth;
+// Add a click event listener to the nav-toggle-button to toggle the menu
+toggleButton.addEventListener("click", toggleNavMenu);
 
-  if (direction === 'prev') {
-    scrollPosition -= scrollAmount;
-    scrollPosition = Math.max(scrollPosition, 0);
+// Add click event listeners to each menu link to close the menu when clicked
+navLinks.forEach((link) => {
+  link.addEventListener("click", toggleNavMenu);
+});
+
+const jobTitleElement = document.getElementById("job-title");
+const cursorElement = document.createElement("span");
+cursorElement.classList.add("cursor");
+cursorElement.textContent = "|";
+jobTitleElement.appendChild(cursorElement);
+const jobTitles = ["Full Stack Web Developer.", "Frontend Developer."];
+
+let currentIndex = 0;
+
+function updateJobTitle() {
+  const currentJobTitle = jobTitles[currentIndex];
+  const typingDelay = 150; // Delay between typing each character
+  const erasingDelay = 100; // Delay before erasing the job title
+
+  let i = 0;
+  let isErasing = false;
+
+  function typeNextCharacter() {
+    if (isErasing) {
+      const updatedJobTitle = currentJobTitle.slice(0, i);
+      jobTitleElement.textContent = updatedJobTitle;
+      i--;
+      if (i === 0) {
+        isErasing = false;
+        currentIndex = (currentIndex + 1) % jobTitles.length;
+        setTimeout(updateJobTitle, 500);
+      } else {
+        setTimeout(typeNextCharacter, erasingDelay);
+      }
+    } else {
+      const updatedJobTitle = currentJobTitle.slice(0, i) + "|";
+      jobTitleElement.textContent = updatedJobTitle;
+      i++;
+      if (i <= currentJobTitle.length) {
+        setTimeout(typeNextCharacter, typingDelay);
+      } else {
+        isErasing = true;
+        cursorElement.style.visibility = "hidden"; // Hide cursor when erasing
+        setTimeout(typeNextCharacter, erasingDelay);
+      }
+    }
+  }
+
+  typeNextCharacter();
+}
+
+// Start the job title animation
+updateJobTitle();
+
+// Add event listener for page load
+window.addEventListener("load", function () {
+  const logo = document.querySelector(".logo");
+  logo.classList.add("visible");
+});
+
+// Callback function when the skills section is intersecting
+function handleSkillsIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const cards = entry.target.querySelectorAll(".skills-card:not(.animate)"); // Select only the cards that are not already animated
+      cards.forEach((card, index) => {
+        card.style.animationDelay = `${index * 0.2}s`; // Add a delay to each card's animation based on its index
+        card.classList.add("animate"); // Add class to trigger the animation
+      });
+      observer.unobserve(entry.target); // Stop observing once the animation is triggered for all cards
+    }
+  });
+}
+
+// Create an intersection observer
+const skillsObserver = new IntersectionObserver(handleSkillsIntersection, {
+  root: null,
+  threshold: 0.5, // Adjust the threshold as needed
+});
+
+// Select the skills section containers
+const skillsSections = document.querySelectorAll("#skills");
+
+// Observe each skills section container
+skillsSections.forEach((skillsSection) => {
+  skillsObserver.observe(skillsSection);
+});
+
+// Helper function to check if an element is in the viewport
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function handleScroll() {
+  skillsSections.forEach((skillsSection) => {
+    if (isInViewport(skillsSection)) {
+      skillsObserver.observe(skillsSection);
+    } else {
+      skillsObserver.unobserve(skillsSection);
+    }
+  });
+}
+
+// Attach the handleScroll function to the scroll event
+window.addEventListener("scroll", handleScroll());
+
+// project animation event
+const projectCards = document.querySelectorAll(".project-card");
+
+function debounce(func, wait = 10, immediate = true) {
+  let timeout;
+  return function () {
+    const context = this,
+      args = arguments;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function checkSlide() {
+  projectCards.forEach((projectCard) => {
+    // Check if the project card is in the viewport
+    const slideInAt =
+      window.scrollY + window.innerHeight - projectCard.offsetHeight / 2;
+    const cardBottom = projectCard.offsetTop + projectCard.offsetHeight;
+    const isHalfShown = slideInAt > projectCard.offsetTop;
+    const isNotScrolledPast = window.scrollY < cardBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      projectCard.classList.add("active");
+    } else {
+      projectCard.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
+
+// Run checkSlide initially to show the project cards in the initial viewport
+checkSlide();
+
+// Add the following JavaScript code
+
+// Function to check if an element is in the viewport
+function isElementInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to handle the scroll event
+function handleScroll() {}
+
+// Event listener for scroll event
+window.addEventListener("scroll", handleScroll);
+
+// dark mode toggle
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+const body = document.body;
+
+darkModeToggle.addEventListener("change", function () {
+  if (darkModeToggle.checked) {
+    enableDarkMode();
   } else {
-    scrollPosition += scrollAmount;
-    scrollPosition = Math.min(scrollPosition, maxScroll);
+    disableDarkMode();
   }
+});
 
-  projectContainer.style.transform = `translateX(-${scrollPosition}px)`;
+function enableDarkMode() {
+  body.classList.add("dark-mode");
+  localStorage.setItem("darkModeEnabled", true);
 }
+
+function disableDarkMode() {
+  body.classList.remove("dark-mode");
+  localStorage.setItem("darkModeEnabled", false);
+}
+
+// Check if dark mode was previously enabled
+const darkModeEnabled = localStorage.getItem("darkModeEnabled");
+if (darkModeEnabled && darkModeEnabled === "true") {
+  darkModeToggle.checked = true;
+  enableDarkMode();
+} else {
+  darkModeToggle.checked = false;
+  disableDarkMode();
+}
+
+// Burger menu toggle
+$(document).ready(function () {
+  var windowWidth = window.innerWidth;
+  var navLinks = $("#nav-links");
+
+  // Function to toggle the navigation menu on mobile screens
+  $("#nav-toggle-button").click(function () {
+    if (windowWidth <= 770) {
+      navLinks.slideToggle();
+    }
+  });
+
+  // Function to close the navigation menu when a link is clicked
+  $("#nav-links a").click(function () {
+    if (windowWidth <= 770) {
+      navLinks.slideUp();
+    }
+  });
+
+  // Add an event listener to handle screen size changes
+  $(window).resize(function () {
+    windowWidth = window.innerWidth;
+
+    // Show or hide the navigation links based on screen size
+    handleScreenSizeChange();
+  });
+
+  // Initial call to handle screen size on page load
+  handleScreenSizeChange();
+
+  // Function to handle screen size changes
+  function handleScreenSizeChange() {
+    // Show or hide the navigation links based on screen size
+    if (windowWidth > 770) {
+      navLinks.show();
+    } else {
+      navLinks.hide();
+    }
+  }
+});
+
+// Function to handle screen size changes
+function handleScreenSizeChange() {
+  if (window.innerWidth > 770) {
+    navList.classList.remove("active"); // Close the burger menu
+    navList.style.display = "flex"; // Display links in a row
+  } else {
+    navList.style.display = "none"; // Hide the navigation links on small screens
+  }
+}
+
+// Add an event listener to handle screen size changes
+window.addEventListener("resize", handleScreenSizeChange);
+
+// Initial call to handle screen size on page load
+handleScreenSizeChange();
+
+var buttonSend = document.getElementById("submit");
+
+buttonSend.addEventListener("click", function () {
+  // Replace 'Badri Vishal Singh' with the desired recipient's name
+  const recipientName = "Badri Vishal Singh";
+
+  // Display a pop-up or alert with the result message
+  alert(
+    `Your message has been sent to ${recipientName}. Thank you for reaching out to me.`
+  );
+});
